93af44456307a1e82479b6a94d4ce8608691333c
hsiaocheng11 <107482377+hsiaocheng11@users.noreply.github.com>
2023-03-09 00:09:35 +0800

    Add files via upload

---
+++ src/core/loader.js
@@ -0,0 +1,50 @@
+import { REST, Routes,Collection } from 'discord.js'
+import fg from 'fast-glob'
+import {useAppStore} from '@/store/app'
+
+const updateSlashCommands = async(commands) => {
+    const rest = new REST({ version: 10 }).setToken(process.env.TOKEN)
+
+    const result = await rest.put(
+        Routes.applicationGuildCommands(
+            process.env.APPLICATION_ID,
+            '986235971190267935'
+        ),
+        {
+            body: commands,
+        }
+    )
+}
+
+export const loadCommands = async () => {
+    const appStore = useAppStore()
+    const commands = []
+    const actions = new Collection()
+    
+    const files = await fg('./src/commands/**/index.js')
+    for (const file of files) {
+        const cmd = await import(file)
+        commands.push(cmd.command)
+        actions.set(cmd.command.name,cmd.action)
+    }
+    await updateSlashCommands(commands)
+    appStore.commandsActionMap = actions
+
+
+}
+
+export const loadEvents = async()=>{
+    const appStore = useAppStore()
+    const client = appStore.client
+    const files = await fg('./src/events/**/index.js')
+    for (const file of files) {
+        const eventFile = await import(file)
+
+        if (eventFile.event){
+            client.once(eventFile.event.name,eventFile.action)
+        }else {
+            client.on(eventFile.event.name,eventFile.action)
+        }
+        
+    }
+}
---
+++ src/core/vue.js
@@ -0,0 +1,8 @@
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
+export default()=>{
+    const vue = createApp({})
+    const pinia = createPinia()
+
+    vue.use(pinia)
+}
